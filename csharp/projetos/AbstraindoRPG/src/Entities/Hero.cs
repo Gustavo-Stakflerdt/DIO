7573755c758d3d564c11b84278672de502894df9
Gustavo Stakflerdt <84250256+Gustavo-Stakflerdt@users.noreply.github.com>
2022-02-08 20:54:11 -0300

    Projetos e exercícios com C#

--- csharp/projetos/AbstraindoRPG/src/Entities/Hero.cs
+++ csharp/projetos/AbstraindoRPG/src/Entities/Hero.cs
@@ -1,10 +1,8 @@
 namespace Abstracao_Jogo_RPG_em_C.src.Entities
 {
 
-    // classe abstrata Hero
     public abstract class Hero
     {
-        // construtor com as características
         public Hero(string Name, int Level, string HeroType){
             this.Name = Name;
             this.Level = Level;
@@ -12,26 +10,20 @@ namespace Abstracao_Jogo_RPG_em_C.src.Entities
 
         }
 
-        // construtor vazio
         public Hero(){
 
         }
 
-        // para poder usar os objetos
         public string Name;
         public int Level;
         public string HeroType;
 
-        // sobrescreve o comportamento padrão com uma mensagem
         public override string ToString(){
             return this.Name + " " + this.Level + " " + this.HeroType;
         }
         
-        // método de ataque 
         public virtual string Attack(){
             return this.Name + " Atacou com a sua espada.";
         }
-
-
     }
 }
