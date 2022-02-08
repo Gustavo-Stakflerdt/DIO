13acddea581657e250fac9f8842cef37464f11bf
Gustavo Stakflerdt <guthriller@gmail.com>
2022-02-08 20:52:52 -0300

    Projetos e exercícios com C#

---
+++ csharp/projetos/AbstraindoRPG/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Abstracao_Jogo_RPG_em_C.src.Entities;
+
+namespace Abstracao_Jogo_RPG_em_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Knight arus = new Knight("Arus", 23, "Knight");
+            Wizard wizard = new Ninja("Jennica", 23, "White Wizard");
+
+            Console.WriteLine(wizard.Attack(1));
+            Console.WriteLine(wizard.Attack(7));
+        }
+    }
+}
