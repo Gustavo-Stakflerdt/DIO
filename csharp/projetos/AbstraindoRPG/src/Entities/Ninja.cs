ce708f2440d9fae78fee2c628640497c52652247
Gustavo Stakflerdt <84250256+Gustavo-Stakflerdt@users.noreply.github.com>
2022-02-08 20:53:43 -0300

    Projetos e exercícios com C#

--- csharp/projetos/AbstraindoRPG/src/Entities/Ninja.cs
+++ csharp/projetos/AbstraindoRPG/src/Entities/Ninja.cs
@@ -1,9 +1,7 @@
 namespace Abstracao_Jogo_RPG_em_C.src.Entities
 {
-    // classe público Ninja que herda a classe Hero
     public class Ninja : Hero
     {
-        // construtor com as características
         public Ninja(string Name, int Level, string HeroType)
         {
             this.Name = Name;
@@ -11,7 +9,6 @@ namespace Abstracao_Jogo_RPG_em_C.src.Entities
             this.HeroType = HeroType;
         }
 
-        // sobrescreve o comportamento padrão com uma mensagem
         public override string Attack(){
              return this.Name + " Atacou com a sua Adaga.";
         }
